8b3ad6623526bd8033a15e67afc8ee1a397e1aed
Arijit Sinha <arijitsinha2020@gmail.com>
2026-06-19 02:39:13 +0530

    Add TwoSum solution
    Implement Two Sum solution using hash map.

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,31 @@
+/*
+Problem: Two Sum
+Platform: LeetCode
+Difficulty: Easy
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mp;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
